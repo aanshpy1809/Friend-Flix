1927389927231cacea49c6e261cc01b5479eb572
Aansh Sagar <sagaraansh@gmail.com>
2024-05-06 04:38:05 +0530

    auth controller completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import ConnectMongoDB from './db/connectMongoDB.js';
-// import authRoutes from './routes/authRoutes.js';
+import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
 const app=express();
 dotenv.config();
@@ -11,8 +11,10 @@ app.get("/",(req,res)=>{
     res.send("App is running")
 });
 app.use(express.json());
+app.use(express.urlencoded({extended: true})) //to parse form data
 app.use(cookieParser());
-// app.use("/api/auth",authRoutes);
+
+app.use("/api/auth",authRoutes);
 app.listen(PORT, ()=>{
     ConnectMongoDB();
     console.log(`Server running on port - ${PORT} `)

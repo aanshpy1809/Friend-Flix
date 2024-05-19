ed2fdbe48682007d97954f4f2bc0d69b1af5fd1e
Aansh Sagar <sagaraansh@gmail.com>
2024-05-20 03:31:49 +0530

    Final deployment code changes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
 import notificationRoutes from './routes/notification.routes.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 import {v2 as cloudinary} from 'cloudinary';
 const app=express();
 dotenv.config();
@@ -15,9 +16,8 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 const PORT=process.env.PORT || 5000;
-app.get("/",(req,res)=>{
-    res.send("App is running")
-});
+const __dirname=path.resolve()
+
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended: true})) //to parse form data
 app.use(cookieParser());
@@ -26,6 +26,16 @@ app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/post",postRoutes);
 app.use("/api/notifications",notificationRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, ()=>{
     ConnectMongoDB();
     console.log(`Server running on port - ${PORT} `)
